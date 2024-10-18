9c44961803e353426b0d1dda36054040c3384dc2
Ved-panchal <vedpanchal99@gmail.com>
2024-10-18 19:10:09 +0530

    Added spec model

---
+++ server/models/specmodels.ts
@@ -0,0 +1,61 @@
+import { DataTypes } from "sequelize";
+import projectModel from "./projectmodel";
+
+const specModel = (sequelize: any) => {
+  const Spec = sequelize.define(
+    "spec",
+    {
+      specName: {
+        type: DataTypes.STRING(50),
+        unique: true,
+        allowNull: false,
+        validate: {
+          is: /^[A-Z0-9]+$/,
+        },
+      },
+      rating: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        validate: {
+          is: /^\d{5}#$/,
+        },
+      },
+      baseMaterial: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      },
+      isDeleted: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+        allowNull: false,
+      },
+      projectId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: projectModel(sequelize),
+          key: 'id',
+        },
+        allowNull: false,
+        onDelete: "CASCADE",
+      },
+    },
+    {
+      timestamps: true,
+      underscored: true,
+      paranoid: true,
+      deletedAt: 'deleted_at',
+    }
+  );
+
+  // Associations
+  Spec.associate = (models: any) => {
+    Spec.belongsTo(models.Project, {
+      foreignKey: "projectId",
+      as: "project",
+    });
+  };
+
+  return Spec;
+};
+
+export default specModel;
