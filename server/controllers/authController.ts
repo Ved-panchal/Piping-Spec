c22783c0652df9221a1edbcaef9b253e0adbe7f6
Ved Panchal <vedpanchal99@gmail.com>
2024-10-26 14:15:45 +0530

    Change the URL

--- server/controllers/authController.ts
+++ server/controllers/authController.ts
@@ -40,7 +40,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
         }
 
         const token = generateJWT({ id: user.id, email: user.email });
-        res.cookie('token', token, { maxAge: 3600000, path: '/', sameSite:'none',secure: true });
+        res.cookie('token', token, { maxAge: 3600000, path: '/', sameSite:'none',secure: true, httpOnly:true });
         
         const { password: _, ...userWithoutPassword } = user.toJSON();
 
